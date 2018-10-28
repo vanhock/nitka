4e64c0735e6ea2ff057d8e0b766ea55e123e1b1c
vanhock <vanhocker@gmail.com>
2018-10-28 12:07:46 +0300

    refactor code

--- index.js
+++ index.js
@@ -1,34 +1,86 @@
 "use strict";
 const cinema = {
   places: [],
-  defaultPrice: 100,
-  order: null,
-  unavailable: getUnavailableIdx(),
+	order: null,
+	unavailable: generateUnavailableIdx(),
   options: {
-    placesId: "places",
-    placeClass: "place"
-  },
-  placesContainer: document.querySelector("#places"),
-  orderBlock: document.querySelector("#order .content")
+		defaultPrice: 100,
+		placeClass: "place",
+		placesContainer: document.querySelector("#places"),
+		orderBlock: document.querySelector("#order .content")
+  }
 };
 
+document.addEventListener("DOMContentLoaded", setPlaces);
+
+function setPlaces() {
+  /** Generate 100 places with 10 random booked places **/
+	let posCounter = 1,
+		rowCounter = 0;
+	for (let i = 0; i < 100; i++) {
+		posCounter++;
+		if (i % 10 === 0) {
+			++rowCounter;
+			posCounter = 1;
+		}
+		cinema.places.push({
+			index: cinema.places.length,
+			position: posCounter,
+			row: rowCounter,
+			price: cinema.options.defaultPrice,
+			available: !cinema.unavailable.some(item => {
+				return item === i + 1;
+			})
+		});
+	}
+	if (cinema.places && cinema.places.length) {
+		renderPlaces();
+	}
+}
+
 function renderPlaces() {
-  if (!cinema.places.length) {
-    return null;
-  }
-  cinema.placesContainer.innerHTML = "";
-  cinema.placesContainer.removeAttribute("data-empty");
-  cinema.places.forEach((p, i) => {
-    const div = document.createElement("div");
-    div.setAttribute("class", cinema.options.placeClass);
-    div.setAttribute("available", p.available);
-    div.setAttribute("data-index", i);
-    div.innerHTML = p.row + "/" + p.position;
-    cinema.placesContainer.appendChild(div);
-  });
-  cinema.placesContainer.childNodes.forEach(elem => {
-    elem.addEventListener("click", togglePlaceInOrder);
-  });
+	if (!cinema.places.length) {
+		return null;
+	}
+	cinema.options.placesContainer.innerHTML = "";
+	cinema.options.placesContainer.removeAttribute("data-empty");
+	cinema.places.forEach((p, i) => {
+		const div = document.createElement("div");
+		div.setAttribute("class", cinema.options.placeClass);
+		div.setAttribute("available", p.available);
+		div.setAttribute("data-index", i);
+		div.innerHTML = p.row + "/" + p.position;
+		cinema.options.placesContainer.appendChild(div);
+	});
+	cinema.options.placesContainer.childNodes.forEach(elem => {
+		elem.addEventListener("click", togglePlaceInOrder);
+	});
+}
+
+function togglePlaceInOrder(e) {
+	const placeIndex = e.target.getAttribute("data-index");
+	const place = cinema.places[placeIndex];
+	let selected = true;
+	if (!place.available) {
+		return null;
+	}
+	if (!cinema.order) {
+		cinema.order = {
+			places: [place],
+			total: place.price
+		};
+	} else {
+		let p = cinema.order.places;
+		if (p.includes(place)) {
+			cinema.order.places.splice(p.indexOf(place), 1);
+			selected = false;
+		} else {
+			cinema.order.places.push(place);
+		}
+	}
+
+	updatePlace(placeIndex, { selected: selected });
+	updateOrder();
 }
 
 function updatePlace(placeIndex, params) {
@@ -40,69 +92,21 @@ function updatePlace(placeIndex, params) {
   }
 }
 
-function setPlaces() {
-  let posCounter = 1,
-    rowCounter = 0;
-  for (let i = 0; i < 100; i++) {
-    posCounter++;
-    if (i % 10 === 0) {
-      ++rowCounter;
-      posCounter = 1;
-    }
-    cinema.places.push({
-      index: cinema.places.length,
-      position: posCounter,
-      row: rowCounter,
-      price: cinema.defaultPrice,
-      available: !cinema.unavailable.some(item => {
-        return item === i + 1;
-      })
-    });
-  }
-  if (cinema.places && cinema.places.length) {
-    renderPlaces();
-  }
-}
-
-function togglePlaceInOrder(e) {
-  const placeIndex = e.target.getAttribute("data-index");
-  const place = cinema.places[placeIndex];
-  let selected = true;
-  if (!place.available) {
-    return null;
-  }
-  if (!cinema.order) {
-    cinema.order = {
-      places: [place],
-      total: place.price
-    };
-  } else {
-    let p = cinema.order.places;
-    if (p.includes(place)) {
-      cinema.order.places.splice(p.indexOf(place), 1);
-      selected = false;
-    } else {
-      cinema.order.places.push(place);
-    }
-  }
-
-  updatePlace(placeIndex, { selected: selected });
-  updateOrder();
-}
+/** Order functions: **/
 
 function updateOrder() {
   if (!cinema.order.places.length) {
     clearOrder();
     return null;
   }
-  cinema.orderBlock.setAttribute("data-empty", "false");
+  cinema.options.orderBlock.setAttribute("data-empty", "false");
 
-  cinema.orderBlock.innerHTML =
+	cinema.options.orderBlock.innerHTML =
     "<div class='order-text'>You chose places:</div>" +
     "<div class='order-places'>" +
     renderOrderPlaces(cinema.order.places) +
     "<div class='order-total'>Total: " +
-    getOrderTotal() +
+		calcOrderTotal() +
     "</div>" +
     "</div>" +
     "<button class='order-buy'>Get Tickets</button><button class='order-cancel'>Cancel</button>";
@@ -123,7 +127,7 @@ function updateOrder() {
     return placesHtml;
   }
 
-  function getOrderTotal() {
+  function calcOrderTotal() {
     return cinema.order.places
       .map(i => {
         return i.price;
@@ -136,10 +140,10 @@ function updateOrder() {
 
 function bookPlaces() {
   cinema.order.places.forEach(p => {
-    updatePlace(p.index, { selected: false, available: false });
+    updatePlace(p.index, { selected: p.available = false, available: p.selected = false });
   });
   cinema.order = null;
-  cinema.orderBlock.innerHTML =
+	cinema.options.orderBlock.innerHTML =
     "<div class='success'>Thank you for the order! </div>";
 }
 
@@ -148,11 +152,13 @@ function clearOrder() {
 		updatePlace(p.index, { selected: false });
 	});
   cinema.order = null;
-  cinema.orderBlock.setAttribute("data-empty", "true");
-  cinema.orderBlock.innerHTML = "";
+	cinema.options.orderBlock.setAttribute("data-empty", "true");
+	cinema.options.orderBlock.innerHTML = "";
 }
 
-function getUnavailableIdx() {
+/** Utilities: **/
+
+function generateUnavailableIdx() {
   const arr = [];
   while (arr.length <= 9) {
     let random = Math.floor(Math.random() * 100) + 1;
@@ -162,4 +168,3 @@ function getUnavailableIdx() {
   return arr;
 }
 
-document.addEventListener("DOMContentLoaded", setPlaces);
