8a40c9581f533f0101dffcb0ab210cd710c740f5
vanhock <vanhocker@gmail.com>
2018-10-28 14:51:20 +0300

    replace string interpolation to es6 style

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 "use strict";
 const cinema = {
   places: [],
-	order: [],
-	unavailable: generateUnavailableIdx(),
+  order: [],
+  unavailable: generateUnavailableIdx(),
   options: {
-		defaultPrice: 100,
-		placeClass: "place",
-		placesContainer: document.querySelector("#places"),
-		orderBlock: document.querySelector("#order .content")
+    defaultPrice: 100,
+    placeClass: "place",
+    placesContainer: document.querySelector("#places"),
+    orderBlock: document.querySelector("#order .content")
   }
 };
 
@@ -15,63 +15,63 @@ document.addEventListener("DOMContentLoaded", setPlaces);
 
 function setPlaces() {
   /** Generate 100 places with 10 random booked places **/
-	for (let row = 1; row <= 10; row++) {
-		for (let column = 1; column <= 10; column++) {
-			let placesCount = cinema.places.length;
-			cinema.places.push({
-				index: placesCount,
-				position: column,
-				row: row,
-				price: cinema.options.defaultPrice,
-				available: !cinema.unavailable.some(item => {
-					return item === placesCount;
-				})
-			});
-		}
-	}
-	renderPlaces();
+  for (let row = 1; row <= 10; row++) {
+    for (let column = 1; column <= 10; column++) {
+      let placesCount = cinema.places.length;
+      cinema.places.push({
+        index: placesCount,
+        position: column,
+        row: row,
+        price: cinema.options.defaultPrice,
+        available: !cinema.unavailable.some(item => {
+          return item === placesCount;
+        })
+      });
+    }
+  }
+  renderPlaces();
 }
 
 function renderPlaces() {
-	if (!cinema.places.length) {
-		return;
-	}
-	cinema.options.placesContainer.innerHTML = "";
-	cinema.options.placesContainer.removeAttribute("data-empty");
-	cinema.places.forEach((p, i) => {
-		const div = document.createElement("div");
-		div.setAttribute("class", cinema.options.placeClass);
-		div.setAttribute("available", p.available);
-		div.setAttribute("data-index", i);
-		div.innerHTML = p.row + "/" + p.position;
-		cinema.options.placesContainer.appendChild(div);
-	});
-	cinema.options.placesContainer.childNodes.forEach(elem => {
-		elem.addEventListener("click", togglePlaceInOrder);
-	});
+  if (!cinema.places.length) {
+    return;
+  }
+  cinema.options.placesContainer.innerHTML = "";
+  cinema.options.placesContainer.removeAttribute("data-empty");
+  cinema.places.forEach((p, i) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", cinema.options.placeClass);
+    div.setAttribute("available", p.available);
+    div.setAttribute("data-index", i);
+    div.innerHTML = p.row + "/" + p.position;
+    cinema.options.placesContainer.appendChild(div);
+  });
+  cinema.options.placesContainer.childNodes.forEach(elem => {
+    elem.addEventListener("click", togglePlaceInOrder);
+  });
 }
 
 function togglePlaceInOrder(e) {
-	const placeIndex = e.target.getAttribute("data-index");
-	const place = cinema.places[placeIndex];
-	let selected = true;
-	if (!place.available) {
-		return;
-	}
-	if (!cinema.order.length) {
-		cinema.order = [place]
-	} else {
-		let p = cinema.order;
-		if (p.includes(place)) {
-			cinema.order.splice(p.indexOf(place), 1);
-			selected = false;
-		} else {
-			cinema.order.push(place);
-		}
-	}
+  const placeIndex = e.target.getAttribute("data-index");
+  const place = cinema.places[placeIndex];
+  let selected = true;
+  if (!place.available) {
+    return;
+  }
+  if (!cinema.order.length) {
+    cinema.order = [place];
+  } else {
+    let p = cinema.order;
+    if (p.includes(place)) {
+      cinema.order.splice(p.indexOf(place), 1);
+      selected = false;
+    } else {
+      cinema.order.push(place);
+    }
+  }
 
-	updatePlace(placeIndex, { selected: selected });
-	updateOrder();
+  updatePlace(placeIndex, { selected: selected });
+  updateOrder();
 }
 
 function updatePlace(placeIndex, params) {
@@ -91,29 +91,25 @@ function updateOrder() {
     return;
   }
   cinema.options.orderBlock.setAttribute("data-empty", "false");
-
-	cinema.options.orderBlock.innerHTML =
-    "<div class='order-text'>You chose places:</div>" +
-    "<div class='order-places'>" +
-    renderOrderPlaces(cinema.order) +
-    "<div class='order-total'>Total: " +
-		calcOrderTotal(cinema.order) +
-    "</div>" +
-    "</div>" +
-    "<button class='order-buy'>Get Tickets</button><button class='order-cancel'>Cancel</button>";
-
+  cinema.options.orderBlock.innerHTML = renderOrder();
   document.querySelector(".order-buy").addEventListener("click", bookPlaces);
   document.querySelector(".order-cancel").addEventListener("click", clearOrder);
 
+  function renderOrder() {
+    return `<div class='order-text'>You chose places:</div>
+    <div class='order-places'>
+			${renderOrderPlaces(cinema.order)}
+    	<div class='order-total'>Total:${calcOrderTotal(cinema.order)}</div>
+    </div>
+    <button class='order-buy'>Get Tickets</button>
+    <button class='order-cancel'>Cancel</button>`;
+  }
   function renderOrderPlaces(places) {
     let placesHtml = "";
     places.forEach(p => {
-      placesHtml +=
-        "<div class='order-place'>Row " +
-        p.row +
-        " position " +
-        p.position +
-        "</div>";
+      placesHtml += `<div class='order-place'>Row ${p.row} position ${
+        p.position
+      }</div>`;
     });
     return placesHtml;
   }
@@ -131,20 +127,23 @@ function updateOrder() {
 
 function bookPlaces() {
   cinema.order.forEach(p => {
-    updatePlace(p.index, { selected: p.selected = false, available: p.available = false });
+    updatePlace(p.index, {
+      selected: (p.selected = false),
+      available: (p.available = false)
+    });
   });
   cinema.order = [];
-	cinema.options.orderBlock.innerHTML =
+  cinema.options.orderBlock.innerHTML =
     "<div class='success'>Thank you for the order! </div>";
 }
 
 function clearOrder() {
-	cinema.order.forEach(p => {
-		updatePlace(p.index, { selected: false });
-	});
+  cinema.order.forEach(p => {
+    updatePlace(p.index, { selected: false });
+  });
   cinema.order = [];
-	cinema.options.orderBlock.setAttribute("data-empty", "true");
-	cinema.options.orderBlock.innerHTML = "";
+  cinema.options.orderBlock.setAttribute("data-empty", "true");
+  cinema.options.orderBlock.innerHTML = "";
 }
 
 /** Utilities: **/
@@ -158,4 +157,3 @@ function generateUnavailableIdx() {
   }
   return arr;
 }
-
